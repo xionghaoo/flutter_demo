8faba00b0c8c52a4171f471cd2631d9f763669fb
xionghao <xionghao930316@gmail.com>
2020-01-30 19:42:06 +0800

    tab切换

--- lib/screen/bottom_tabs.dart
+++ lib/screen/bottom_tabs.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/theme/colors.dart';
 
 class BottomTabsPage extends StatefulWidget {
 
@@ -23,6 +24,25 @@ class _BottomTabsPageState extends State<BottomTabsPage> {
     });
   }
 
+  BottomNavigationBarItem _bottomNavigationBarItem(int index, IconData icon, String title) {
+    return BottomNavigationBarItem(
+      icon: _iconForIndex(index, icon),
+      title: _textForIndex(index, title)
+    );
+  }
+
+  Icon _iconForIndex(int index, IconData icon) {
+    return currentSelectedPage == index
+        ? Icon(icon, color: primaryColor, size: 28)
+        : Icon(icon, color: Colors.grey);
+  }
+
+  Text _textForIndex(int index, String title) {
+    return currentSelectedPage == index
+        ? Text(title, style: TextStyle(color: primaryColor, fontSize: 15))
+        : Text(title, style: TextStyle(color: Colors.grey, fontSize: 14));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,27 +51,14 @@ class _BottomTabsPageState extends State<BottomTabsPage> {
         child: tabs.elementAt(currentSelectedPage),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home, color: Colors.grey,),
-            title: Text("Home"),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business, color: Colors.grey),
-            title: Text("Business")
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search, color: Colors.grey),
-            title: Text("Search")
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person, color: Colors.grey),
-            title: Text("Profile")
-          )
+        items: <BottomNavigationBarItem>[
+          _bottomNavigationBarItem(0, Icons.home, "Home"),
+          _bottomNavigationBarItem(1, Icons.business, "Business"),
+          _bottomNavigationBarItem(2, Icons.search, "Search"),
+          _bottomNavigationBarItem(3, Icons.person, "Profile")
         ],
         type: BottomNavigationBarType.fixed,
         currentIndex: currentSelectedPage,
-        selectedItemColor: Colors.red[500],
         onTap: _onItemTapped,
       ),
     );
