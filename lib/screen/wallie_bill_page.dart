e0df771388be05b1e2388b5d28143d16539c491c
xionghao <xionghao930316@gmail.com>
2020-02-06 15:52:02 +0800

    add refresh widget

--- lib/screen/wallie_bill_page.dart
+++ lib/screen/wallie_bill_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class WallieBillPage extends StatefulWidget {
   @override
@@ -7,6 +8,31 @@ class WallieBillPage extends StatefulWidget {
 }
 
 class _WallieBillPageState extends State<WallieBillPage> {
+
+  RefreshController _refreshController = RefreshController(initialRefresh: false);
+
+  void _onRefresh() async{
+    // monitor network fetch
+//    print("_onRefresh start");
+    await Future.delayed(Duration(milliseconds: 1000));
+//    print("_onRefresh end ");
+    // if failed,use refreshFailed()
+    _refreshController.refreshCompleted();
+  }
+
+  void _onLoading() async{
+    // monitor network fetch
+//    print("_onLoading start");
+    await Future.delayed(Duration(milliseconds: 1000));
+    // if failed,use loadFailed(),if no data return,use LoadNodata()
+//    items.add((items.length+1).toString());
+//    print("_onLoading end");
+    if(mounted)
+      setState(() {
+
+      });
+    _refreshController.loadComplete();
+  }
 
   Widget _toolbar() {
     return Container(
@@ -43,51 +69,59 @@ class _WallieBillPageState extends State<WallieBillPage> {
           children: <Widget>[
             _toolbar(),
             Expanded(
-              child: ListView.builder(
-                itemCount: 20,
-                itemBuilder: (context, index) => Padding(
-                  padding: const EdgeInsets.only(left: 20, right: 10),
-                  child: Column(
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          Text("${index + 1} Jan 2020", style: TextStyle(color: Colors.green[500], fontWeight: FontWeight.w500, fontSize: 15),),
-                          Expanded(child: SizedBox(),),
-                          Container(
-                            width: 60,
-                            height: 60,
-                            decoration: BoxDecoration(
-                              color: Color.fromRGBO(204, 204, 204, 0.2),
-                              borderRadius: BorderRadius.circular(10)
+              child: SmartRefresher(
+                enablePullDown: true,
+                enablePullUp: false,
+                controller: _refreshController,
+                onRefresh: _onRefresh,
+                onLoading: _onLoading,
+                header: WaterDropHeader(),
+                child: ListView.builder(
+                  itemCount: 20,
+                  itemBuilder: (context, index) => Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 10),
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Text("${index + 1} Jan 2020", style: TextStyle(color: Colors.green[500], fontWeight: FontWeight.w500, fontSize: 15),),
+                            Expanded(child: SizedBox(),),
+                            Container(
+                              width: 60,
+                              height: 60,
+                              decoration: BoxDecoration(
+                                color: Color.fromRGBO(204, 204, 204, 0.2),
+                                borderRadius: BorderRadius.circular(10)
+                              ),
+                              child: Center(
+                                child: Icon(Icons.add_shopping_cart),
+                              ),
                             ),
-                            child: Center(
-                              child: Icon(Icons.add_shopping_cart),
+                            SizedBox(width: 20,),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text("Netfix", style: TextStyle(color: Colors.grey[500], fontSize: 12),),
+                                Text("\$10.00", style: TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),)
+                              ],
                             ),
-                          ),
-                          SizedBox(width: 20,),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text("Netfix", style: TextStyle(color: Colors.grey[500], fontSize: 12),),
-                              Text("\$10.00", style: TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),)
-                            ],
-                          ),
-                          SizedBox(width: 30,),
-                          Container(
-                            width: 40,
-                            height: 40,
-                            child: Center(
-                              child: Icon(Icons.chevron_right, color: Colors.grey[400],),
-                            ),
-                          )
-                        ],
-                      ),
-                      Divider(color: Color.fromRGBO(200, 200, 200, 0.5), endIndent: 10, thickness: 0.3,)
-                    ],
-                  ),
-                )
+                            SizedBox(width: 30,),
+                            Container(
+                              width: 40,
+                              height: 40,
+                              child: Center(
+                                child: Icon(Icons.chevron_right, color: Colors.grey[400],),
+                              ),
+                            )
+                          ],
+                        ),
+                        Divider(color: Color.fromRGBO(200, 200, 200, 0.5), endIndent: 10, thickness: 0.3,)
+                      ],
+                    ),
+                  )
+                ),
               ),
             )
           ],
