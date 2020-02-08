eddea29a9c935a6741efe6c858b10dcbdf6a0e03
xionghao <xionghao930316@gmail.com>
2020-02-08 17:49:56 +0800

    ios native call flutter

--- ios/Runner/BuildConfigPlugin.swift
+++ ios/Runner/BuildConfigPlugin.swift
@@ -9,18 +9,39 @@ import Flutter
 import UIKit
 
 class BuildConfigPlugin: NSObject, FlutterPlugin {
+    
+    private var channel: FlutterMethodChannel
+    private var controller: UIViewController
+    
+    init(registrar: FlutterPluginRegistrar) {
+        self.controller = (UIApplication.shared.delegate?.window?!.rootViewController)!
+        self.channel = FlutterMethodChannel(name: "xh.zero/version", binaryMessenger: registrar.messenger())
+    }
+    
     public static func register(with registrar: FlutterPluginRegistrar) {
-        let channel = FlutterMethodChannel(name: "xh.zero/version", binaryMessenger: registrar.messenger())
-        let instance = BuildConfigPlugin()
-        registrar.addMethodCallDelegate(instance, channel: channel)
+        //        let channel = FlutterMethodChannel(name: "xh.zero/version", binaryMessenger: registrar.messenger())
+        let instance = BuildConfigPlugin(registrar: registrar)
+        registrar.addMethodCallDelegate(instance, channel: instance.channel)
     }
     
     func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         if (call.method == "getApplicationVersion") {
             // 获取应用版本
             result(Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String)
+            
+            self.channel.invokeMethod("callFlutter", arguments: nil, result: { result in
+                print("result is: \(type(of: result!))") //__NSCFString
+                let alert = UIAlertController(title: "Received", message: "\(result!)", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+                    
+                }))
+                self.controller.present(alert, animated: true, completion: nil)
+            })
         } else {
             result(FlutterMethodNotImplemented)
         }
+        
     }
+    
+    
 };
